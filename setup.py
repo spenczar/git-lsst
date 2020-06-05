d484c9402372eb5efcc1f81b94edb8a959145993
Spencer Nelson <swnelson@uw.edu>
2020-06-05 10:54:20 -0700

    Prompt the user to make sure they did a jenkins build

--- setup.py
+++ setup.py
@@ -1,23 +1,3 @@
 from setuptools import setup
 
-install_requires = ['jira', 'PyGithub', 'giturlparse']
-
-dev_requires = ['mypy', 'flake8', 'flake8-mypy', 'black']
-
-setup(name='git-lsst',
-      version="0.2.1",
-      description="Git plugin for working on LSST",
-      url='https://github.com/spenczar/git-lsst',
-      classifiers=[
-          "Programming Language :: Python :: 3",
-          "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
-          "Development Status :: 3 - Alpha",
-          "Operating System :: POSIX :: Linux",
-      ],
-      author='Spencer Nelson',
-      author_email='swnelson@uw.edu',
-      license='MIT',
-      install_requires=install_requires,
-      extras_require={"dev": dev_requires},
-      scripts=["git-lsst"],
-      zip_safe=False)
+setup()
